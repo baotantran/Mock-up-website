f436ed34c44f7fdcabdea41c7c3f2f18f4bccd1b
baotantran <baotantran92@gmail.com>
2018-08-19 21:37:15 -0700

    testing jquery function

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,3 +4,9 @@ $(document).ready(function() {
         $(this).css('background-color', '#ff0000')
     })
 })
+
+$(document).ready(function() {
+    $('nav').click(function() {
+        $(this).css('background-color', '#00ff00')
+    })
+})
